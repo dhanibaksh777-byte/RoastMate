6c9a20bd7cdbbeb9cf718bcb90d23da67b50aa7b
Dhani Baksh <dhanibaksh781@gmail.com>
2026-07-10 01:04:54 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -26,16 +26,6 @@ class HistoryResponse(BaseModel):
 
 
 
-# from pydantic import BaseModel
-# from typing import Optional
 
-
-# class ChatRequest(BaseModel):
-#     message : str
-#     session_id : Optional[str] = None
-    
-# class ChatResponse(BaseModel):
-#     response : str
-#     session_id : str
 
     
