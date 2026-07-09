59881d6931523d8b869ad97101f5279b39e8ec95
Dhani Baksh <dhanibaksh781@gmail.com>
2026-07-10 01:35:15 +0500

    Update groq_client.py

--- groq_client.py
+++ groq_client.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 
 client = Groq(api_key=os.getenv("groq_api_key"))
-def get_roast(message : str):
+def get_roast(message : list):
     response = client.chat.completions.create(
         model = "llama-3.3-70b-versatile",
         messages=message
